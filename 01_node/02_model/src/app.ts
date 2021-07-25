f46f0de3f60635bcaa2345f689e1dcae828ec1bf
t2 <oo_ri@naver.com>
2021-07-25 15:46:10 +0900

    02_mode app.model.ts Update

--- 01_node/02_model/src/app.ts
+++ 01_node/02_model/src/app.ts
@@ -1,14 +1,12 @@
 import express from 'express';
 
+import { Cat } from './app.model';
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.post('/', (req, res) => {
-  res.send({ name: 'Node.js' });
+  res.send({ cats: Cat });
 });
 
 app.listen(port, () => {
