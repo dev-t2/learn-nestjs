227781b1d5da1d6f7d85246f12fe8befc5f2c03e
t2 <oo_ri@naver.com>
2021-07-25 12:00:39 +0900

    01_setup src/app.ts Path Update

--- 01_node/01_setup/src/app.ts
+++ 01_node/01_setup/src/app.ts
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.post('/', (req, res) => {
+  res.send({ name: 'Node.js' });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
