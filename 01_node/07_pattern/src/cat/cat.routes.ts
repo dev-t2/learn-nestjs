55915098b2863a2060473f257ebe088642ca5a85
t2 <oo_ri@naver.com>
2021-07-25 20:26:53 +0900

    07_pattern Update

---
+++ 01_node/07_pattern/src/cat/cat.routes.ts
@@ -0,0 +1,98 @@
+import { Router } from 'express';
+
+import { Cat } from './cat.model';
+
+const router = Router();
+
+router.post('/', (req, res) => {
+  try {
+    const id = Cat[Cat.length - 1].id + 1;
+    const data = { id, ...req.body };
+
+    Cat.push(data);
+
+    res.send({ isSuccess: true, data });
+  } catch (error) {
+    res.status(400).send({ isSuccess: false, error: error.message });
+  }
+});
+
+router.get('/', (req, res) => {
+  try {
+    const cats = Cat;
+
+    res.send({ isSuccess: true, data: cats });
+  } catch (error) {
+    res.status(400).send({ isSuccess: false, error: error.message });
+  }
+});
+
+router.get('/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+    const cat = Cat.find(cat => cat.id === parseInt(id, 10));
+
+    if (cat) {
+      res.send({ isSuccess: true, data: cat });
+    } else {
+      res.status(404).send({ isSuccess: false, error: 'Cat Not Found' });
+    }
+  } catch (error) {
+    res.status(400).send({ isSuccess: false, error: error.message });
+  }
+});
+
+router.put('/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const index = Cat.findIndex(cat => cat.id === parseInt(id, 10));
+
+    if (index !== -1) {
+      Cat[index] = data;
+
+      res.send({ isSuccess: true });
+    } else {
+      res.status(404).send({ isSuccess: false, error: 'Cat Not Found' });
+    }
+  } catch (error) {
+    res.status(400).send({ isSuccess: false, error: error.message });
+  }
+});
+
+router.patch('/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const index = Cat.findIndex(cat => cat.id === parseInt(id, 10));
+
+    if (index !== -1) {
+      Cat[index] = { ...Cat[index], ...data };
+
+      res.send({ isSuccess: true });
+    } else {
+      res.status(404).send({ isSuccess: false, error: 'Cat Not Found' });
+    }
+  } catch (error) {
+    res.status(400).send({ isSuccess: false, error: error.message });
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+    const index = Cat.findIndex(cat => cat.id === parseInt(id, 10));
+
+    if (index !== -1) {
+      Cat.splice(index, 1);
+
+      res.send({ isSuccess: true });
+    } else {
+      res.status(404).send({ isSuccess: false, error: 'Cat Not Found' });
+    }
+  } catch (error) {
+    res.status(400).send({ isSuccess: false, error: error.message });
+  }
+});
+
+export default router;
