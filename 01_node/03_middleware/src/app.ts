b45a574bc6fd487012c854c94702d7f217ab8e06
t2 <oo_ri@naver.com>
2021-07-25 16:30:50 +0900

    Global, Router and Not Found Middleware Update

--- 01_node/03_middleware/src/app.ts
+++ 01_node/03_middleware/src/app.ts
@@ -5,8 +5,39 @@ import { Cat } from './app.model';
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+  console.log('Global Middleware');
+
+  next();
+});
+
 app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/cats', (req, res) => {
   res.send({ cats: Cat });
+});
+
+app.get(
+  '/cats/:id',
+  (req, res, next) => {
+    console.log('Router Middleware');
+
+    next();
+  },
+  (req, res) => {
+    const { id } = req.params;
+    const cat = Cat.find(cat => cat.id === parseInt(id, 10));
+
+    res.send(cat);
+  }
+);
+
+app.use((req, res) => {
+  console.log('Not Found Middleware');
+
+  res.status(404).send('Not Found');
 });
 
 app.listen(port, () => {
