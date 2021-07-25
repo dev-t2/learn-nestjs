2dc43ba0ae514f4466cbf1626f93b1abdd2f1788
t2 <oo_ri@naver.com>
2021-07-25 17:45:10 +0900

    05_refactoring Express Router Update

--- 01_node/05_refactoring/src/app.ts
+++ 01_node/05_refactoring/src/app.ts
@@ -1,49 +1,13 @@
 import express from 'express';
 
-import { Cat } from './app.model';
+import catRouter from './cat/cat.routes';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.post('/cat', (req, res) => {
-  try {
-    const id = Cat[Cat.length - 1].id + 1;
-    const data = { id, ...req.body };
-
-    Cat.push(data);
-
-    res.send({ isSuccess: true, data });
-  } catch (error) {
-    res.status(400).send({ isSuccess: false, error: error.message });
-  }
-});
-
-app.get('/cats', (req, res) => {
-  try {
-    const cats = Cat;
-
-    res.send({ isSuccess: true, data: cats });
-  } catch (error) {
-    res.status(400).send({ isSuccess: false, error: error.message });
-  }
-});
-
-app.get('/cats/:id', (req, res) => {
-  try {
-    const { id } = req.params;
-    const cat = Cat.find(cat => cat.id === parseInt(id, 10));
-
-    if (cat) {
-      res.send({ isSuccess: true, data: cat });
-    } else {
-      res.status(404).send({ isSuccess: false, error: 'Cat Not Found' });
-    }
-  } catch (error) {
-    res.status(400).send({ isSuccess: false, error: error.message });
-  }
-});
+app.use('/cat', catRouter);
 
 app.use((req, res) => {
   res.status(404).send('Not Found');
