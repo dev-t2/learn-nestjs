f39c068a775298f425e90e7d3e2143558f075e7d
t2 <oo_ri@naver.com>
2021-07-25 17:46:15 +0900

    06_api Update

---
+++ 01_node/06_api/src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+import catRouter from './cat/cat.routes';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use('/cat', catRouter);
+
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
