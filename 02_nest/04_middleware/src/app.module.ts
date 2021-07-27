72b17591376c08115260415093c99b95b353a731
t2 <oo_ri@naver.com>
2021-07-28 00:15:41 +0900

    Logger Middleware Update

--- 02_nest/04_middleware/src/app.module.ts
+++ 02_nest/04_middleware/src/app.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { CatModule } from './cat/cat.module';
 
 @Module({
   imports: [CatModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
