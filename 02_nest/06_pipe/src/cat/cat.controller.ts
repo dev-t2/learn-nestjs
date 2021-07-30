09565e82bad3bb716e66edcc1689d209ec6b5c21
t2 <oo_ri@naver.com>
2021-07-30 23:36:53 +0900

    06_pipe Update

---
+++ 02_nest/06_pipe/src/cat/cat.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+
+import { CatService } from './cat.service';
+
+@Controller('cat')
+export class CatController {
+  constructor(private readonly catService: CatService) {}
+
+  @Post()
+  createCat(): string {
+    return 'Create Cat';
+  }
+
+  @Get()
+  getAllCat(): string {
+    return 'Get All Cat';
+  }
+
+  @Get(':id')
+  getCat(): string {
+    return 'Get Cat';
+  }
+
+  @Put(':id')
+  updateCat(): string {
+    return 'Update Cat';
+  }
+
+  @Delete(':id')
+  deleteCat(): string {
+    return 'Delete Cat';
+  }
+}
