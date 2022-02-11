43d25abe82f2e44d48f572c4f3610c944227168b
developer-t2 <oo_ri@naver.com>
2022-02-11 17:47:24 +0900

    07-parameter Complete

--- 02-controller/07-parameter/src/app.controller.ts
+++ 02-controller/07-parameter/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get(':userId/post/:postId')
+  getHello(
+    @Param('userId') userId: string,
+    @Param('postId') postId: string,
+  ): string {
+    console.log({ userId, postId });
+
     return this.appService.getHello();
   }
 }
