1e5e258151853212f2d31ee4aaf1b6bb81594f38
developer-t2 <oo_ri@naver.com>
2022-02-11 15:17:01 +0900

    learn-nestjs Repository Reset

--- 02_nest/06_pipe/src/common/pipe/positive-int.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PositiveIntPipe } from './positive-int.pipe';
-
-describe('PositiveIntPipe', () => {
-  it('should be defined', () => {
-    expect(new PositiveIntPipe()).toBeDefined();
-  });
-});
