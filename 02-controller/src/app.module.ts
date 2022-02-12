96fdc0d3151106f67ab43454dc218e683cee2b93
t2 <oo_ri@naver.com>
2022-02-12 20:39:36 +0900

    02-controller Sub Domain Update

--- 02-controller/src/app.module.ts
+++ 02-controller/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ApiController } from './api/api.controller';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [ApiController, AppController],
   providers: [AppService],
 })
 export class AppModule {}
