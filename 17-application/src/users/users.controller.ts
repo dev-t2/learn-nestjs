8934d293ee1dfd57a5164b2411e78667173d73d9
developer-t2 <oo_ri@naver.com>
2022-02-14 16:10:08 +0900

    17-application Create User Update

--- 17-application/src/users/users.controller.ts
+++ 17-application/src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+  }
+}
