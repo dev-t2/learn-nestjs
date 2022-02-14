680d0b7a32a1ea7844ed514e5182f2ed692a454d
developer-t2 <oo_ri@naver.com>
2022-02-14 17:24:26 +0900

    17-application User Controller Update

--- 17-application/src/users/dto/sign-up.dto.ts
+++ 17-application/src/users/dto/sign-up.dto.ts
@@ -1,4 +1,4 @@
-export class CreateUserDto {
+export class SignUpDto {
   readonly name: string;
   readonly email: string;
   readonly password: string;
--- 17-application/src/users/users.controller.ts
+++ 17-application/src/users/users.controller.ts
@@ -1,10 +1,34 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+import { VerifyEmailDto } from './dto/verify-email.dto';
+import { SignUpDto } from './dto/sign-up.dto';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Controller('users')
 export class UsersController {
-  @Post()
-  async createUser(@Body() createUserDto: CreateUserDto) {
-    console.log(createUserDto);
+  @Post('email-verification')
+  async verifyEmail(@Body() verifyEmailDto: VerifyEmailDto): Promise<string> {
+    console.log(verifyEmailDto);
+
+    return;
+  }
+
+  @Post('sign-up')
+  async signUp(@Body() signUpDto: SignUpDto): Promise<void> {
+    console.log(signUpDto);
+  }
+
+  @Post('sign-in')
+  async signIn(@Body() signInDto: SignInDto): Promise<string> {
+    console.log(signInDto);
+
+    return;
+  }
+
+  @Get(':id')
+  async readUserInfo(@Param('id') userId: string): Promise<void> {
+    console.log(userId);
+
+    return;
   }
 }
