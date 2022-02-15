f072a4e7a6628bed59ec2d309cfe1048d5047f1e
developer-t2 <oo_ri@naver.com>
2022-02-15 11:05:40 +0900

    16-application Controller Update

--- 16-application/src/users/users.controller.ts
+++ 16-application/src/users/users.controller.ts
@@ -2,19 +2,22 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { VerifyEmailDto } from './dto/verify-email.dto';
 import { SignUpDto } from './dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('sign-up')
+  async signUp(@Body() signUpDto: SignUpDto): Promise<void> {
+    console.log(signUpDto);
+  }
+
   @Post('email-verification')
   async verifyEmail(@Body() verifyEmailDto: VerifyEmailDto): Promise<string> {
     console.log(verifyEmailDto);
 
     return;
-  }
-
-  @Post('sign-up')
-  async signUp(@Body() signUpDto: SignUpDto): Promise<void> {
-    console.log(signUpDto);
   }
 
   @Post('sign-in')
