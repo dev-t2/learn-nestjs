bf651c2780a60667c111c29d006c9eea2aebbc6c
t2 <oo_ri@naver.com>
2022-02-21 19:52:20 +0900

    04-middleware Logger Middleware Update

--- 04-middleware/src/app.module.ts
+++ 04-middleware/src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { CatsModule } from './cats/cats.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [UsersModule, CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- 04-middleware/src/logger.middleware.ts
+++ 04-middleware/src/logger.middleware.ts
@@ -1,8 +1,18 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  private readonly logger = new Logger(LoggerMiddleware.name);
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const { ip, method, originalUrl } = req;
+    const { statusCode } = res;
+
+    res.on('finish', () => {
+      this.logger.log(`${ip} ${method} ${originalUrl} ${statusCode}`);
+    });
+
     next();
   }
 }
