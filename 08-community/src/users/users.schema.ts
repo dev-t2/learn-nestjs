afd906663c76c9f2670b577ca5bad24e9d5e9dd9
t2 <oo_ri@naver.com>
2022-02-22 19:53:29 +0900

    08-community Users Schema Update

---
+++ 08-community/src/users/users.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+const options: SchemaOptions = {
+  timestamps: true,
+};
+
+@Schema(options)
+export class User {
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true, unique: true })
+  username: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop()
+  avatar: string;
+}
+
+export type UserDocument = User & Document;
+
+export const UserSchema = SchemaFactory.createForClass(User);
