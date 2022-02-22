fbaa22ac84a6a21eef63c0640476654528a5d5ae
t2 <oo_ri@naver.com>
2022-02-22 19:08:11 +0900

    08-community Configuration Install and Setup

--- 08-community/src/app.module.ts
+++ 08-community/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [MongooseModule.forRoot('')],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
