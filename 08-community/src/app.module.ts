250e3cbf29e468fbeecbea5415052c4918e5d5fb
t2 <oo_ri@naver.com>
2022-02-22 19:23:09 +0900

    08-community Users Module and Controller Setup

--- 08-community/src/app.module.ts
+++ 08-community/src/app.module.ts
@@ -2,11 +2,17 @@ import { Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB_URI)],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_URI),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
