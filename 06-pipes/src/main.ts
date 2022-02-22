b62e7db29dd41876d0f5dd19068c6d51f419da69
t2 <oo_ri@naver.com>
2022-02-22 16:09:02 +0900

    06-pipes Filters Folder Update and Pipes Setup

--- 06-pipes/src/main.ts
+++ 06-pipes/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './commons/exception/http-exception.filter';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
