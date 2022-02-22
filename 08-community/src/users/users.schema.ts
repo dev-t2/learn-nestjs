8f74b21ee9ce9afeb1abb7a3e0d1fb8569836e2d
t2 <oo_ri@naver.com>
2022-02-22 20:10:59 +0900

    08-community Class Validator Update

--- 08-community/src/users/users.schema.ts
+++ 08-community/src/users/users.schema.ts
@@ -1,5 +1,12 @@
 import { Prop, Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 const options: SchemaOptions = {
   timestamps: true,
@@ -7,15 +14,26 @@ const options: SchemaOptions = {
 
 @Schema(options)
 export class User {
+  @IsNotEmpty()
+  @IsEmail()
   @Prop({ required: true, unique: true })
   email: string;
 
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(10)
   @Prop({ required: true, unique: true })
   username: string;
 
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(12)
   @Prop({ required: true })
   password: string;
 
+  @IsString()
   @Prop()
   avatar: string;
 }
