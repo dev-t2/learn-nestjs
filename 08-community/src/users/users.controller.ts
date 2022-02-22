03d1b7788e2085724303b6269c203a8e7f02d9e7
t2 <oo_ri@naver.com>
2022-02-22 19:36:08 +0900

    08-community App Module Reset and Users Controller Update

--- 08-community/src/users/users.controller.ts
+++ 08-community/src/users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getUser() {
+    return 'GetUser';
+  }
+
+  @Post('signup')
+  signup() {
+    return 'signup';
+  }
+
+  @Post('login')
+  login() {
+    return 'login';
+  }
+
+  @Post('logout')
+  logout() {
+    return 'logout';
+  }
+
+  @Post('upload')
+  upload() {
+    return 'upload';
+  }
+}
