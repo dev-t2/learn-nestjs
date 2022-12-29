204d2fcade3d5e3debae5a67a65d743f8400b5c6
dev-t2 <oo_ri@naver.com>
2022-12-29 23:20:43 +0900

    Users Controller Update

---
+++ src/users/dto/index.ts
@@ -0,0 +1,3 @@
+export { CreateUserDto } from './create-user.dto';
+export { LoginDto } from './login.dto';
+export { VerifyEmailDto } from './verify-email.dto';
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+
+import { CreateUserDto, LoginDto, VerifyEmailDto } from './dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+
+    return;
+  }
+
+  @Post('email-verify')
+  async verifyEmail(@Query() verifyEmailDto: VerifyEmailDto) {
+    console.log(verifyEmailDto);
+
+    return;
+  }
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    console.log(loginDto);
+
+    return;
+  }
+
+  @Get(':id')
+  async getUser(@Param('id') id: string) {
+    console.log(id);
+
+    return;
+  }
+}
