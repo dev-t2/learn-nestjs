aec057bc30adc7fd2445b7613a4ece642bb47a0f
dev-t2 <oo_ri@naver.com>
2023-01-01 15:33:22 +0900

    Email Service Nodemailer Setup

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  private transporter: Mail;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: { user: process.env.GMAIL_USER, pass: process.env.GMAIL_PASS },
+    });
+  }
+
+  async sendEmail(email: string, token: string) {
+    return await this.transporter.sendMail({
+      to: email,
+      subject: '가입 인증 메일',
+      html: `
+        확인 버튼을 누르시면 가입 인증이 완료됩니다. </br>
+
+        <form action="${process.env.BASE_URL}/users/email-verify?token=${token}" method="post">
+          <button>확인</button>
+        </form>
+      `,
+    });
+  }
+}
