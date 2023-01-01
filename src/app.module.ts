a572d8a174727e9f5788fcd969b13acc0a0908df
dev-t2 <oo_ri@naver.com>
2023-01-01 17:46:16 +0900

    NestJS Configuration Install and Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [ConfigModule.forRoot(), UsersModule],
 })
 export class AppModule {}
