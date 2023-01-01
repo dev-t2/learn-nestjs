5af73e78a9cf1e65d1ed1c1059dabb58780c26cc
dev-t2 <oo_ri@naver.com>
2023-01-01 15:57:49 +0900

    Users Service Nodemailer Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
-import { EmailService } from './email/email.service';
 
 @Module({
   imports: [],
   controllers: [UsersController],
-  providers: [UsersService, EmailService],
+  providers: [UsersService],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import Mail from 'nodemailer/lib/mailer';
+import nodemailer from 'nodemailer';
+import { v4 as uuidv4 } from 'uuid';
 
 import { CreateUserDto } from './dto';
 
 @Injectable()
 export class UsersService {
-  async createUser(createUserDto: CreateUserDto) {
-    return createUserDto;
+  private transporter: Mail;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: { user: process.env.GMAIL_USER, pass: process.env.GMAIL_PASS },
+    });
+  }
+
+  async createUser({ email }: CreateUserDto) {
+    const token = uuidv4();
+
+    const result = await this.transporter.sendMail({
+      to: email,
+      subject: '가입 인증 메일',
+      html: `
+        확인 버튼을 누르시면 가입 인증이 완료됩니다. </br>
+
+        <form action="${process.env.BASE_URL}/users/email-verify?token=${token}" method="post">
+          <button>확인</button>
+        </form>
+      `,
+    });
+
+    return result;
   }
 }
