4238d680e75a43c4b5a2eb6ee78a79fa4aa9d32a
dev-t2 <oo_ri@naver.com>
2024-03-14 01:20:06 +0900

    Cats DTO Update

--- 02-controllers/src/cats/cats.controller.ts
+++ 02-controllers/src/cats/cats.controller.ts
@@ -1,9 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+
+import { CreateCatDto, FindCatsDto, UpdateCatDto } from './cats.dto';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  createCat(@Body() createCatDto: CreateCatDto) {
+    return `Create Cat - Name: ${createCatDto.name}`;
+  }
+
   @Get()
-  findCats() {
-    return 'Find Cats';
+  findCats(@Query() findCatsDto: FindCatsDto) {
+    return `Find Cats - Limit: ${findCatsDto.limit}`;
+  }
+
+  @Get(':id')
+  findCat(@Param('id') id: number) {
+    return `Find Cat ${id}`;
+  }
+
+  @Put(':id')
+  updateCat(@Param('id') id: number, @Body() updateCatDto: UpdateCatDto) {
+    return `Update Cat ${id} - Name: ${updateCatDto.name}`;
+  }
+
+  @Delete(':id')
+  deleteCat(@Param('id') id: number) {
+    return `Delete Cat ${id}`;
   }
 }
